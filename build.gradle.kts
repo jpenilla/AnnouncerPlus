b6d0b2d020e9c6a86bc90fc0046bca63752213a1
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2023-06-13 13:59:51 -0700

    Dependency updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -49,7 +49,7 @@ dependencies {
   implementation("org.spongepowered", "configurate-extra-kotlin")
 
   implementation("io.insert-koin", "koin-core", "3.4.2")
-  implementation("xyz.jpenilla", "legacy-plugin-base", "0.0.1+80-SNAPSHOT")
+  implementation("xyz.jpenilla", "legacy-plugin-base", "0.0.1+83-SNAPSHOT")
   implementation("org.bstats", "bstats-bukkit", "3.0.2")
   implementation("io.papermc", "paperlib", "1.0.8")
 
@@ -110,7 +110,7 @@ tasks {
     dependsOn(shadowJar)
   }
   runServer {
-    minecraftVersion("1.20")
+    minecraftVersion("1.20.1")
   }
   withType<RunServer> {
     javaLauncher.set(
